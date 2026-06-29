b43fe1d8a55bddf77f536128ff67b90e1e5726de
Igbined <93369396+Igbined@users.noreply.github.com>
2026-06-29 20:48:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-    apiEndpoint: 'https://telegram-activation-bot--igbinedion109.replit.app/api/HHOJG'
+    apiEndpoint: 'https://telegram-activation-bot--igbinedion109.replit.app/api/ZRATZ'
 };
