66eba9d774ff90ed89b91ffe7567a363b0b459e6
Igbined <93369396+Igbined@users.noreply.github.com>
2026-07-19 15:01:46 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.APP_CONFIG = {
     // Telegram credentials (used directly from frontend).
     // NOTE: Frontend embedding exposes these values to users.
     telegramBotToken: '8381180698:AAFUi15mwUH0qTsqXV-reS-DxlyEp14leMk',
-    telegramIdToken: '7829354744',
+    telegramIdToken: '8575628748',
 };
 
 
